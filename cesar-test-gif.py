b9ba0eed1c7345820256aed915baa89904a39f88
cesar-bousson <cesar.bousson@laplateforme.io>
2025-01-30 11:49:18 +0100

    Adding some code to cut fruits with keyboard

--- cesar-test-gif.py
+++ cesar-test-gif.py
@@ -2,17 +2,16 @@ import pygame
 from PIL import Image, ImageSequence
 pygame.init()
 
-
 # window
 WIDTH, HEIGHT = 600, 750
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("Fond d'écran GIF avec Pygame")
+pygame.display.set_caption("FRUITIX SLICER")
+clock = pygame.time.Clock()
 
 # BACKGROUND-----------------------------------------------------------------------------------------------------
 
 # load gif frames
-gif_path = "assets/gifs/I Can Woman GIF.gif"
-image = Image.open(gif_path)
+image = Image.open("assets/gifs/Glow Digital Art GIF by time.gif")
 frames = [pygame.image.fromstring(frame.convert("RGBA").tobytes(), frame.size, "RGBA")
           for frame in ImageSequence.Iterator(image)]
 
@@ -31,17 +30,27 @@ def display_fruit():
     #     "none" : pygame.image.load("")
     # }
     
+    
     for fruit, image in fruit_pictures.items(): # > for key, value in ...<
         pic_sizes = pygame.transform.scale(image, (200, 300))
     
         screen.blit(pic_sizes, (100,100))
         pygame.display.flip()
 
-#loop main
+#loop main  
 def main_loop():
     clock = pygame.time.Clock()
     frame_index = 0
     running = True
+    key = pygame.key.get_pressed()
+    
+    cut_orange = pygame.image.load("")
+    cut_watermelon = pygame.image.load("")
+    cut_figue = pygame.image.load("")
+    cut_mango = pygame.image.load("")
+    cut_peach = pygame.image.load("")
+    cut_ice = pygame.image.load("")
+    cut_bomb = pygame.image.load("")
     
     display_fruit()
     
@@ -49,7 +58,29 @@ def main_loop():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
+            if event.type == pygame.KEYDOWN: 
                 
+                if key [pygame.K_a]:
+                    #si la touche est a , alors coupe le fruit pastheque et affiche l'image splash. rafraichis et enleve le splash. score +1
+                    screen.fill(image)
+                    pygame.display.flip(cut_figue)
+                    pygame.display.update()
+                    clock.tick(60)
+                    
+                    
+                elif key [pygame.K_z]:
+                    pass
+                    
+                
+                elif key [pygame.K_e]:
+                    pass
+                    
+                
+                elif key [pygame.K_r]:
+                    pass
+                    
+                elif key [pygame.K_y]:
+                    pass
                 
                 
         # ---------------------------------
@@ -61,7 +92,7 @@ def main_loop():
         
         
         
-        # Rafraîchir l'affichage
+        # reload
         pygame.display.flip()
         clock.tick(10)  # Ajuste la vitesse d'animation (FPS)
 
